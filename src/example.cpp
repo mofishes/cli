8777cf7ca488a4b9db3469ddba17e012b8827a91
木杉 <zhmushan@qq.com>
2020-01-27 18:27:43 +0800

    add tools (#1)
    * add tools
    
    * update ci

--- src/example.cpp
+++ src/example.cpp
@@ -1,11 +1,5 @@
 #include "example.h"
 
-double add(int f1, int f2)
-{
-    return f1 + f2;
-}
+double add(int f1, int f2) { return f1 + f2; }
 
-double mul(int f1, int f2){
-    return f1*f2;
-}
-
+double mul(int f1, int f2) { return f1 * f2; }
